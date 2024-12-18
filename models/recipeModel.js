c28337873ae6ee4ce71b7284c650ce7e4f8b67df
nimansu-fernando <nimansufernando@gmail.com>
2024-12-18 11:03:48 +0530

    NC21GD-62 Add New method for creating a recipe in model

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -101,4 +101,104 @@ const getAllRecipesPaginated = (limit = 21, page = 1, callback) => {
   });
 };
 
+const createRecipe = (recipeData, callback) => {
+  // Start a transaction to handle multiple table insertions
+  db.beginTransaction((err) => {
+    if (err) {
+      return callback(err, null);
+    }
+
+    // Insert main recipe data
+    const recipeQuery = `
+      INSERT INTO recipes 
+      (title, description, servings, prep_time, cook_time, instructions, cooks_tips, image_url) 
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    const recipeValues = [
+      recipeData.title,
+      recipeData.description,
+      recipeData.servings,
+      `${recipeData.prepTime.hours} hours ${recipeData.prepTime.minutes} minutes`,
+      `${recipeData.cookTime.hours} hours ${recipeData.cookTime.minutes} minutes`,
+      JSON.stringify(recipeData.instructions),
+      recipeData.cooksTips || null,
+      recipeData.imageUrl || null
+    ];
+
+    db.query(recipeQuery, recipeValues, (recipeErr, recipeResult) => {
+      if (recipeErr) {
+        return db.rollback(() => {
+          callback(recipeErr, null);
+        });
+      }
+
+      const recipeId = recipeResult.insertId;
+
+      // Insert ingredients
+      const ingredientQueries = recipeData.ingredients.map(ingredient => {
+        return new Promise((resolve, reject) => {
+          const ingredientQuery = `
+            INSERT INTO recipe_ingredients 
+            (rec_id, quantity, measurement, ingredient_name) 
+            VALUES (?, ?, ?, ?)
+          `;
+          
+          db.query(
+            ingredientQuery, 
+            [recipeId, ingredient.quantity, ingredient.measurement, ingredient.item], 
+            (ingredientErr) => {
+              if (ingredientErr) reject(ingredientErr);
+              else resolve();
+            }
+          );
+        });
+      });
+
+      // Insert categories if provided
+      const categoryQueries = recipeData.categories ? 
+        recipeData.categories.map(category => {
+          return new Promise((resolve, reject) => {
+            const categoryQuery = `
+              INSERT INTO recipe_categories 
+              (rec_id, cat_id) 
+              SELECT ?, cat_id 
+              FROM categories 
+              WHERE cname = ?
+            `;
+            
+            db.query(
+              categoryQuery, 
+              [recipeId, category], 
+              (categoryErr) => {
+                if (categoryErr) reject(categoryErr);
+                else resolve();
+              }
+            );
+          });
+        }) : 
+        [];
+
+      // Execute all queries
+      Promise.all([...ingredientQueries, ...categoryQueries])
+        .then(() => {
+          db.commit((commitErr) => {
+            if (commitErr) {
+              return db.rollback(() => {
+                callback(commitErr, null);
+              });
+            }
+            callback(null, { id: recipeId, ...recipeData });
+          });
+        })
+        .catch((error) => {
+          return db.rollback(() => {
+            callback(error, null);
+          });
+        });
+    });
+  });
+};
+
+
 module.exports = { getRecipes, getCategories, getAllRecipesPaginated };
